f0ed208c9f0db6ac52bd5e8a5fb55d7271b0b628
Elnicoc0 <nicola.bazillio@viacesi.fr>
2016-11-07 10:01:09 +0100

    ON A CHANGÉ DES TRUCS

--- main.c
+++ main.c
@@ -6,12 +6,14 @@ int addition (int a, int b)
     return a+b;
 }
 
+int soustraction (int a, int b)
+{
+    return a-b;
+}
+
 int main ()
 {
-<<<<<<< HEAD
-    printf("a+b=%d\n", addition(1, 4));
-=======
     printf("L'addition de a et b vaut %d\n", addition(1, 4));
->>>>>>> origin/master
+    printf("la soustraction de a et b vaut %d\n", soustraction(1, 4));
     return 0;
 }
